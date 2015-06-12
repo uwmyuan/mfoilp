b35abfc4802d86c476e462074b3e574cde0300ed
James Cussens <james.cussens@york.ac.uk>
2015-06-12 12:41:32 +0200

    added in code for creating linear constraints

--- mercury_lib.m
+++ mercury_lib.m
@@ -15,6 +15,7 @@
 :- import_module string.
 
 :- type atom_store.
+:- type lexp_int.
 
 :- pred makevars(atom_store::out,
 		 list(int)::out,
@@ -23,14 +24,21 @@
 		 list(float)::out,
 		 list(int)::out,
 		 list(float)::out) is det.
+
 :- pred usevars(atom_store::in,io::di, io::uo) is det.
+
+:- pred makelincons(atom_store::in,
+		    list(float)::out,
+		    list(lexp_int)::out,
+		    list(float)::out) is det.
+		    
 
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
 
 :- implementation.
 
-:- import_module map.
+:- import_module bimap.
 :- import_module solutions.
 
 :- type person ---> alice; bob; charlie; dean; ed.
@@ -42,10 +50,24 @@
 	; cb1(int,person)
 	; cb2(int,person,person).
 
-:- type atom_store == map(int,atom).
+:- type atom_store == bimap(int,atom).
+
+:- type lterm ---> (float * atom).
+:- type lterm_int ---> (float * int).
+:- type lexp == list(lterm).
+:- type lexp_int == list(lterm_int).
+%:- type lpsol == map(atom,float).
+
+:- type lincons ---> lincons(float,lexp,float).
+:- type lincons_int ---> lincons(float,lexp_int,float).
 
 
 %-----------------------------------------------------------------------------%
+
+:- pred lincons(lincons::out) is multi.
+
+lincons(lincons(-100.0,[-1.0 * cb1(1,X), 1.0 * smokes(X), -1.0 * cancer(X)],0.0))  :-
+	person(X).
 
 
 :- pred atom(atom::out) is multi.
@@ -80,11 +102,30 @@ vartype(_) = 1.
 name(_) = "todo".
 %name(Atom) = Name :- Name = "", io.write(Atom,Name,!IO).
 
+makelincons(AtomStore,Lbs,Lexp_Int,Ubs) :-
+	solutions(lincons,AllLinCons),
+	convert_lc(AtomStore,AllLinCons,Lbs,Lexp_Int,Ubs).
+
+:- pred convert_lc(atom_store::in,list(lincons)::in,list(float)::out,list(lexp_int)::out,list(float)::out) is det.
+
+convert_lc(_AtomStore,[],[],[],[]).
+convert_lc(AtomStore,[lincons(Lb,LinExpr,Ub)|T],[Lb|LbT],[LinExpr_I|LET],[Ub|UbT]) :-
+	convert_le(AtomStore,LinExpr,LinExpr_I),
+	convert_lc(AtomStore,T,LbT,LET,UbT).
+
+:- pred convert_le(atom_store::in,lexp::in,lexp_int::out) is det.
+
+convert_le(_AtomStore,[],[]).
+convert_le(AtomStore,[F * Atom|T],[F * I|T2]) :-
+	I = bimap.reverse_lookup(AtomStore,Atom),
+	convert_le(AtomStore,T,T2).
+
+
 :- pragma foreign_export("C", makevars(out,out,out,out,out,out,out), "makevars").
 
 makevars(AtomStore,Idents,Names,Lbs,Ubs,VarTypes,Objs) :-
 	solutions(atom,AllAtoms),
-	map.init(AS0),
+	bimap.init(AS0),
 	store_atoms(AllAtoms,Idents,Names,Lbs,Ubs,VarTypes,Objs,0,AS0,AtomStore).
 
 :- pred store_atoms(list(atom)::in,
@@ -99,14 +140,15 @@ makevars(AtomStore,Idents,Names,Lbs,Ubs,VarTypes,Objs) :-
 store_atoms([],[],[],[],[],[],[],_,!AS).
 store_atoms([H|T],[I|IT],[name(H)|NT],[lb(H)|LT],[ub(H)|UT],
 	    [vartype(H)|VT],[objective(H)|OT],I,!AS) :-
-	map.det_insert(I,H,!AS),
+	bimap.det_insert(I,H,!AS),
 	store_atoms(T,IT,NT,LT,UT,VT,OT,I+1,!AS).
 
 
 :- pragma foreign_export("C", usevars(in,di,uo), "usevars").
 
 usevars(AtomStore,!IO) :-
-	map.keys_and_values(AtomStore, Keys, Values),
+	bimap.ordinates(AtomStore, Values),
+	bimap.coordinates(AtomStore, Keys),
 	write_all(Keys,!IO),
 	write_all(Values,!IO).
 
