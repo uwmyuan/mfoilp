f97cdfd4dd9669eab7518f188f79585447c01b0a
James Cussens <james.cussens@york.ac.uk>
2015-07-11 13:37:23 +0200

    working static example

---
+++ shortest_path.m
@@ -0,0 +1,86 @@
+:- module prob.
+:- interface.
+
+%----------------------------------------------------------------------%
+
+
+
+:- import_module mercury_lib.
+
+:- type atom.
+
+:- pred atom(atom::out) is multi.
+:- pred initial_constraint(lincons::out) is nondet.
+
+:- func objective(atom) = float.
+:- func lb(atom) = float.
+:- func ub(atom) = float.
+:- func vartype(atom) = vartype.
+
+%----------------------------------------------------------------------%
+
+:- implementation.
+
+:- import_module list.
+:- import_module int.
+:- import_module solutions.
+
+% define atom type
+
+:- type node == int.
+:- type edge ---> e(node,node).
+:- type atom == edge.
+
+
+% define atom-variable generator
+
+:- pred node(node::out) is multi.
+node(X) :- node_gen(1,X).
+
+:- pred node_gen(node::in,node::out) is multi.
+node_gen(X,X).
+node_gen(X,Y) :- X < 8, node_gen(X+1,Y).
+
+
+:- pred edge(edge::out) is multi.
+edge(e(1,2)).
+edge(e(X,Y)) :- node(X), node(Y), not (X=1,Y=2), not X = Y.
+
+
+atom(X) :- edge(X).
+
+objective(_Atom) = 1.0.
+lb(_Atom) = 0.0.
+ub(_Atom) = 1.0.
+vartype(_Atom) = binary.
+
+% constraints
+
+:- pred source(node::out) is det.
+source(4).
+
+
+:- pred target(node::out) is det.
+target(8).
+
+initial_constraint(lincons(finite(1.0),LinExpr,finite(1.0))) :-
+	source(X),
+	Out = (pred(1.0 * E::out) is nondet :- edge(E), E = e(X,_)),
+	solutions(Out,LinExpr).
+
+initial_constraint(lincons(finite(1.0),LinExpr,finite(1.0))) :-
+	target(X),
+	In = (pred(1.0 * E::out) is nondet :- edge(E), E = e(_,X)),
+	solutions(In,LinExpr).
+
+initial_constraint(lincons(finite(0.0),LinExpr,finite(0.0))) :-
+	node(X),
+	not source(X),
+	not target(X),
+	In =  (pred(1.0 * E::out) is nondet :- edge(E), E=e(_,X)),
+	solutions(In,InEdges),
+	Out = (pred(-1.0 * E::out) is nondet :- edge(E), E = e(X,_)),
+	solutions(Out,OutEdges),
+	append(InEdges,OutEdges,LinExpr).
+
+
