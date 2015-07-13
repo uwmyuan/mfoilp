56293756b109d71a130338527240d1384aa7802f
James Cussens <james.cussens@york.ac.uk>
2015-07-13 17:31:50 +0200

    now using fact tables

--- scores2m.py
+++ scores2m.py
@@ -7,6 +7,8 @@ import re
 vpat = re.compile(r'^([^-\s]+)\s+\d+\s*$')
 spat = re.compile(r'^(-[\d.]+)\s+\d+\s+(.*)')
 
+fact_table = True
+palim = 3
 for line in fileinput.input():
     result = vpat.match(line)
     if result:
@@ -14,6 +16,17 @@ for line in fileinput.input():
         continue
     result = spat.match(line)
     if result:
-        print 'score({2},[{1}],{0}).'.format(result.group(1),','.join(result.group(2).split()),thisvar)
+        if fact_table:
+            parents = result.group(2).split()
+            while len(parents) < 3:
+                parents.append('-1')
+            print 'score_table({0},{1},{2},{3},{4}).'.format(
+                thisvar,
+                parents[0],
+                parents[1],
+                parents[2],
+                result.group(1))
+        else:
+            print 'score({2},[{1}],{0}).'.format(result.group(1),','.join(result.group(2).split()),thisvar)
 
     
