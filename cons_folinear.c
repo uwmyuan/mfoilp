4facf76d54dbd2a29e4a2911393ea3c1a668b4ca
James Cussens <james.cussens@york.ac.uk>
2015-09-28 09:25:59 +0200

    debug cut printing enabled

--- cons_folinear.c
+++ cons_folinear.c
@@ -4,7 +4,7 @@
  */
 
 /*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
-/*#define SCIP_DEBUG*/
+#define SCIP_DEBUG
 #include <assert.h>
 #include <string.h>
 
@@ -477,6 +477,9 @@ SCIP_DECL_CONSENFOLP(consEnfolpFolinear)
 
          SCIP_CALL( SCIPcreateEmptyRowCons(scip, &row, conshdlr, "cut", 1.0, SCIPinfinity(scip), FALSE, FALSE, TRUE) );
          SCIP_CALL( SCIPaddVarsToRowSameCoef(scip, row, nvars, clausevars, 1.0) );
+#ifdef SCIP_DEBUG
+         SCIPdebug( SCIPprintRow(scip, row, NULL) );
+#endif
          SCIP_CALL( SCIPaddCut(scip, NULL, row, FALSE, &cutoff) );
          SCIP_CALL( SCIPreleaseRow(scip, &row));
          nGen++;
