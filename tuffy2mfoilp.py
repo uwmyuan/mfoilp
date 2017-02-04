d8c9c631151d681e88489f0d124a501f18f3a4e1
James Cussens <james.cussens@york.ac.uk>
2017-02-04 07:54:17 +0000

    compiles now with neg weighted clauses and type generation
    now need to get rid of fact tables

--- tuffy2mfoilp.py
+++ tuffy2mfoilp.py
@@ -52,7 +52,14 @@ def updatetypes(match):
 def getpred(lit):
     match = fact_pattern.match(lit)
     return match.group(1)
-    
+
+def update_grounded(grounded,lit):
+    match = fact_pattern.match(lit)
+    predsym = match.group(1)
+    for i, x in enumerate(match.group(2).split(',')):
+        if x not in grounded and x[0].isupper():
+            grounded[x] = type_dkt[predsym][i]
+
 def getargs(lit):
     match = fact_pattern.match(lit)
     return frozenset([x for x in match.group(2).split(',') if x[0].isupper()])
@@ -76,7 +83,86 @@ def to_lp(lit):
         else:
             outargs.append('"{0}"'.format(arg))
     return "{0}({1})".format(match.group(1),','.join(outargs))
-            
+
+
+def process_clause(neglits,poslits,foclausenum,cblit):
+        mc_lits = ['clause("{0}")'.format(foclausenum)]
+        # 'grounded' maps each variable to its type
+        grounded = {}
+        cl1.append('clause("{0}").'.format(foclausenum))
+        for neglit in neglits:
+            if not is_cwa(neglit):
+                if not getargs(neglit).issubset(grounded):
+                    mc_lits.append('insol({0})'.format(neglit))
+                mc_lits.append('neglit({0})'.format(neglit))
+                nl.append('neglit("{0}",{1}).'.format(foclausenum,neglit))
+                atom_types.add('{0}({1})'.format(getpred(neglit),','.join(['string']*nargs(neglit))))
+                update_grounded(grounded,neglit)
+        for neglit in neglits:
+            if is_cwa(neglit):
+                mc_lits.append('{{{0}}}'.format(neglit))
+                inout = []
+                match = fact_pattern.match(neglit)
+                for a in match.group(2).split(','):
+                    if a[0].isupper() and a not in grounded:
+                        inout.append('out')
+                    else:
+                        inout.append('in')
+                try:
+                    modes[getpred(neglit)].add(tuple(inout))
+                except KeyError:
+                    modes[getpred(neglit)] = set([tuple(inout)])
+                update_grounded(grounded,neglit)
+        for poslit in poslits:
+            if is_cwa(poslit):
+                match = fact_pattern.match(poslit)
+                predsym = match.group(1)
+                args = match.group(2).split(',')
+                for i, arg in enumerate(args):
+                    if arg[0].isupper() and arg not in grounded:
+                        mc_lits.append('{{{0}({1})}}'.format(type_dkt[predsym][i],arg))
+                mc_lits.append('{{not {0}}}'.format(poslit))
+                inout = ['in']*len(args)
+                try:
+                    modes[getpred(poslit)].add(tuple(inout))
+                except KeyError:
+                    modes[getpred(poslit)] = set([tuple(inout)])
+                update_grounded(grounded,poslit)
+        for poslit in poslits:
+            if not is_cwa(poslit):
+                match = fact_pattern.match(poslit)
+                predsym = match.group(1)
+                args = match.group(2).split(',')
+                for i, arg in enumerate(args):
+                    if arg[0].isupper() and arg not in grounded:
+                        mc_lits.append('{{{0}({1})}}'.format(type_dkt[predsym][i],arg))
+                mc_lits.append('poslit({0})'.format(poslit))
+                pl.append('poslit("{0}",{1}).'.format(foclausenum,poslit))
+                atom_types.add('{0}({1})'.format(getpred(poslit),','.join(['string']*nargs(poslit))))
+                update_grounded(grounded,poslit)
+        sorted_grounded = sorted(grounded)
+        if cblit is None:
+            cblitargs = sorted_grounded
+            cblit = 'cb({0},{1})'.format(foclausenum,','.join(cblitargs))
+            atom_types.add('cb(int,{0})'.format(','.join(['string']*len(grounded))))
+            # this is wrong, need to get the correct types!!
+            type_dkt['cb{0}'.format(foclausenum)] = [grounded[x] for x in sorted_grounded]
+        else:
+            match = fact_pattern.match(cblit)
+            args = match.group(2).split(',')
+            predsym = 'cb{0}'.format(args[0])
+            args = args[1:]
+            for i, arg in enumerate(args):
+                if arg[0].isupper() and arg not in grounded:
+                    mc_lits.append('{{{0}({1})}}'.format(type_dkt[predsym][i],arg))
+        mc_lits.append('poslit({0})'.format(cblit))
+        pl.append('poslit("{0}",{1}).'.format(foclausenum,cblit))
+        this_clause = '{0} -->\n'.format(mc_lits[0])
+        for lit in mc_lits[1:-1]:
+              this_clause += '  {0},\n'.format(lit)
+        this_clause += '  {0}.\n'.format(mc_lits[-1])
+        return this_clause, cblit
+
 current_predicate = None
 fact_table_decl = []
 fobjs = {}
@@ -116,6 +202,9 @@ for line in mln:
 
     if line == '':
         continue
+
+    if 'EXIST' in line:
+        continue
     
     if line[:2] == '//':
         continue
@@ -146,74 +235,39 @@ for line in mln:
         #print(line,poslits,neglits)
         weight = float(match.group(1))
         if weight > 0:
-            mc_lits = ['clause("{0}")'.format(foclausenum)]
-            grounded = set()
-            cl1.append('clause("{0}").'.format(foclausenum))
-            for neglit in neglits:
-                if not is_cwa(neglit):
-                    if not getargs(neglit).issubset(grounded):
-                        mc_lits.append('insol({0})'.format(neglit))
-                    mc_lits.append('neglit({0})'.format(neglit))
-                    nl.append('neglit("{0}",{1}).'.format(foclausenum,neglit))
-                    atom_types.add('{0}({1})'.format(getpred(neglit),','.join(['string']*nargs(neglit))))
-                    grounded.update(getargs(neglit))
-            for neglit in neglits:
-                if is_cwa(neglit):
-                    mc_lits.append('{{{0}}}'.format(neglit))
-                    inout = []
-                    match = fact_pattern.match(neglit)
-                    for a in match.group(2).split(','):
-                        if a[0].isupper() and a not in grounded:
-                            inout.append('out')
-                        else:
-                            inout.append('in')
-                    try:
-                        modes[getpred(neglit)].add(tuple(inout))
-                    except KeyError:
-                        modes[getpred(neglit)] = set([tuple(inout)])
-                    grounded.update(getargs(neglit))
-            for poslit in poslits:
-                if is_cwa(poslit):
-                    match = fact_pattern.match(poslit)
-                    predsym = match.group(1)
-                    args = match.group(2).split(',')
-                    for i, arg in enumerate(args):
-                        if arg[0].isupper() and arg not in grounded:
-                            mc_lits.append('{{{0}({1})}}'.format(type_dkt[predsym][i],arg))
-                    mc_lits.append('{{not {0}}}'.format(poslit))
-                    inout = ['in']*len(args)
-                    try:
-                        modes[getpred(poslit)].add(tuple(inout))
-                    except KeyError:
-                        modes[getpred(poslit)] = set([tuple(inout)])
-                    grounded.update(getargs(poslit))
-            for poslit in poslits:
-                if not is_cwa(poslit):
-                    match = fact_pattern.match(poslit)
-                    predsym = match.group(1)
-                    args = match.group(2).split(',')
-                    for i, arg in enumerate(args):
-                        if arg[0].isupper() and arg not in grounded:
-                            mc_lits.append('{0}({1})'.format(predsym,type_dkt[predsym][i]))
-                    mc_lits.append('poslit({0})'.format(poslit))
-                    pl.append('poslit("{0}",{1}).'.format(foclausenum,poslit))
-                    atom_types.add('{0}({1})'.format(getpred(poslit),','.join(['string']*nargs(poslit))))
-                    grounded.update(getargs(poslit))
-            cblit = 'cb({0},{1})'.format(foclausenum,','.join(sorted(grounded)))
-            atom_types.add('cb(int,{0})'.format(','.join(['string']*len(grounded))))
-            mc_lits.append('poslit({0})'.format(cblit))
-            pl.append('poslit("{0}",{1}).'.format(foclausenum,cblit))
-
-            this_clause = '{0} -->\n'.format(mc_lits[0])
-            for lit in mc_lits[1:-1]:
-                  this_clause += '  {0},\n'.format(lit)
-            this_clause += '  {0}.\n'.format(mc_lits[-1])
-            clauses.append(this_clause)
-            objectives.append('objective({0},{1}).'.format(cblit,weight))
+            clause, cblit = process_clause(neglits,poslits,foclausenum,None)
+            clauses.append(clause)
             foclausenum += 1
+            objectives.append('objective({0},{1}).'.format(cblit,weight))
+        elif weight < 0:
+            # use this to get the cblit
+            clause, cblit = process_clause(neglits,poslits,foclausenum,None)
+            for neglit in neglits:
+                clause, cblit = process_clause([],[neglit],foclausenum,cblit)
+                clauses.append(clause)
+                foclausenum += 1
+            for poslit in poslits:
+                clause, cblit = process_clause([poslit],[],foclausenum,cblit)
+                clauses.append(clause)
+                foclausenum += 1
+            objectives.append('objective({0},{1}).'.format(cblit,-weight))
+            
+# now collect constants of various types
 
-#print(atom_types)
-
+evidence = open(sys.argv[2])
+constants = {}
+for line in evidence:
+    match = fact_pattern.match(line)
+    if not match:
+        continue
+    types = type_dkt[match.group(1)]
+    for i, constant in enumerate(match.group(2).split(',')):
+        try:
+            constants[types[i]].add(constant)
+        except KeyError:
+            constants[types[i]] = set([constant])
+evidence.close()
+            
 print(header)
 print('% define atom type')
 print(':- type atom --->')
@@ -255,8 +309,10 @@ for x in nl:
     print(x)
 print()
 
-print('% dummy definition, will be removed')
-print(':- pred person(string::out) is nondet.')
-print('person(_) :- fail.')
-
-
+for typ, konstants in constants.items():
+    print(':- pred {0}(string).'.format(typ))
+    print(':- mode {0}(out) is multi.'.format(typ))
+    print(':- mode {0}(in) is semidet.'.format(typ))
+    for k in sorted(konstants):
+        print('{0}("{1}").'.format(typ,k.strip()))
+    print()
