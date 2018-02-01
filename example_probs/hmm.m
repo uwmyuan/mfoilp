566d0fa7fac97665eb965d7c8c06fe22380954a5
James Cussens <james.cussens@york.ac.uk>
2018-02-01 12:56:11 +0000

    made HMM example consistent with that of PRISM

--- example_probs/hmm.m
+++ example_probs/hmm.m
@@ -1,3 +1,5 @@
+% implements Viterbi calculation for the hmm.psm example
+% which comes with PRISM
 :- module prob.
 :- interface.
 
@@ -25,43 +27,43 @@
 
 :- import_module int.
 :- import_module float.
+:- import_module math.
 
 :- type obs ---> a ; b.
-:- type state ---> s1 ; s2.
+:- type state ---> init ; s0 ; s1.
 
 :- type atom ---> at_state(int,state) ; trans(int,state,state) ; emit(int,state,obs).
 
-objective(emit(_,s1,a),2.0).
-objective(emit(_,s1,b),12.0).
-objective(emit(_,s2,a),2.0).
-objective(emit(_,s2,b),1.0).
+objective(Atom,-ln(Prob)) :- probs(Atom,Prob). 
 
-objective(trans(_,s1,s1),2.0).
-objective(trans(_,s1,s2),12.0).
-objective(trans(_,s2,s1),2.0).
-objective(trans(_,s2,s2),1.0).
+:- pred probs(atom::in,float::out) is semidet.
+
+probs(emit(_,s0,a),0.5).
+probs(emit(_,s0,b),0.5).
+probs(emit(_,s1,a),0.6).
+probs(emit(_,s1,b),0.4).
+
+probs(trans(_,init,s0),0.9).
+probs(trans(_,init,s1),0.1).
+probs(trans(_,s0,s0),0.2).
+probs(trans(_,s0,s1),0.8).
+probs(trans(_,s1,s0),0.8).
+probs(trans(_,s1,s1),0.2).
 
 
-:- pred state(state::out) is multi.
-state(s1).
-state(s2).
-
-:- pred obs(int,obs).
-:- mode obs(out,out) is multi.
-:- mode obs(in,out) is semidet.
+:- pred obs(int::in,obs::out) is semidet.
 
 obs(1,a).
 obs(2,a).
 obs(3,a).
 obs(4,a).
 obs(5,a).
-obs(6,a).
+obs(6,b).
 obs(7,b).
 obs(8,b).
-obs(9,a).
+obs(9,b).
 obs(10,b).
-obs(11,b).
-obs(12,a).
+
 
 clause("emit").
 clause("emit") -->
@@ -74,16 +76,19 @@ poslit("emit",emit(_,_,_)).
 
 clause("dyn").
 clause("dyn") -->
-    {obs(I,_Obs)},
-    poslit(at_state(I,s1)),
-    poslit(at_state(I,s2)).
-poslit("dyn",at_state(_,_)).
+    insol(at_state(I,State)),
+    neglit(at_state(I,State)),
+    {obs(I+1,_Obs)},
+    poslit(trans(I,State,s0)),
+    poslit(trans(I,State,s1)).
+neglit("dyn",at_state(_,_)).
+poslit("dyn",trans(_,_,_)).
 
 clause("trans").
 clause("trans") -->
     insol(at_state(I,State)),
     neglit(at_state(I,State)),
-    {state(State2)},
+    insol(trans(I,State,State2)),
     neglit(trans(I,State,State2)),
     poslit(at_state(I+1,State2)).
 neglit("trans",at_state(_,_)).
@@ -91,7 +96,7 @@ neglit("trans",trans(_,_,_)).
 poslit("trans",at_state(_,_)).
 
 initial_clause("start") -->
-	initial_poslit(at_state(0,s1)).
+	initial_poslit(at_state(0,init)).
 
     
 
